10939fd1399a60e93ec442e3ceeb8da5db695496
GBTC441010UR <GBTC441010UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-01-28 16:22:15 -0500

    #2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,9 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-    for(int i = 1; i <= 10; i++) {
-        System.out.println(i);
-    }
+//    for(int i = 1; i <= 10; i++) {
+//        System.out.println(i);
+//    }
+//     for(int i = 10; i > 0; i--) {
+//         System.out.println(i);
+//       }
+
+
+
 
     }
 }
