b3bafc4617e4c97607e1059a3025493c2216c4db
GBTC441010UR <GBTC441010UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-01-28 16:48:58 -0500

    #seven if next

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,21 +6,23 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+//#1
 //    for(int i = 1; i <= 10; i++) {
 //        System.out.println(i);
 //    }
+//#2        
 //     for(int i = 10; i > 0; i--) {
 //         System.out.println(i);
 //       }
-
+//#3
 //       for (int i = 1; i <= 20; i+=2){
 //           System.out.println(i);
 //       }
-
+//#4
 //        for (int i = 2; i <=20; i+=2){
 //            System.out.println(i);
 //        }
-
+//#5
 //        Scanner sc = new Scanner(System.in);
 //        int num;
 //        System.out.println("Enter number: ");
@@ -28,8 +30,13 @@ public class Main {
 //        for (int i = num; i >= 1; i--) {
 //            System.out.println(i);
 //        }
-
-
+ // #6
+//        int total = 0;
+//        for (int i= 1; i <=10; i++ ) {
+//            total += i;
+//        }
+//        System.out.println(total);
+//
 
 
     }
